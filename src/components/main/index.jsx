105f756cfa6cd34a7e25fe3c239471deecb78270
Auto Commit <auto@local>
2026-06-11 10:30:46 -0300

    feat: create axios-mob

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,20 +1,40 @@
+import { useEffect, useState } from "react";
 import { Card } from "../card";
 import "./style.css";
+import axios from 'axios';
 export const Main = () => {
+
+  const [personagens, setPersonagens] = useState([]);
+
+  //     useEffect(() => {
+  //     fetch("https://dragonball-api.com/api/characters?limit=58")
+  //     .then((res)=> res.json())
+  //     .then(data => {
+  //       console.log(data.items);
+  //       setPersonagens(data.items);
+  //     })  
+  //     .catch((error) => console.error(error))
+  //   }, [])
+
+  useEffect(() => {
+      axios.get("https://dragonball-api.com/api/characters?limit=58")
+      .then(res => 
+        {setPersonagens(res.data.items)})
+      .catch(err => console.log(err))
+  },[])
+
   return (
+
     <main>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+        {personagens.map((personagem) => (
+          <Card
+            image={personagem.image}
+            title={personagem.name}
+            description={personagem.description}
+          />
+
+        ))}
+
     </main>
   );
 };
